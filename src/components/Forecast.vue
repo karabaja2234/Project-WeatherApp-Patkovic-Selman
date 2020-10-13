39db2cebe0b935db0770eda459a6127ca26619c9
karabaja2234 <selmanpatak@gmail.com>
2020-10-13 02:08:40 +0200

    Implemented printing out the forecast data into custom boxes

--- src/components/Forecast.vue
+++ src/components/Forecast.vue
@@ -1,15 +1,31 @@
 <template>
   <div class="Forecast" :style="{ backgroundImage: 'url(' + require('@/images/forecastbg1.jpg') + ')', 
                         backgroundRepeat: 'no-repeat', 
-                        backgroundSize: 'cover', 
+                        backgroundSize: '1920px 1080px', 
                         width: '100%', 
-                        height: '100vh', 
+                        minHeight: '100vh', 
                         padding: '0', 
                         margin: '0'}"
   >
-  <div class="container">
-    <div class="row bg-dark" v-for="(city, index) in shownForecast" v-bind:key="index">
-
+  <div class="container d-flex flex-wrap justify-content-around" style="padding-top: 3%;">
+    <div class="row d-flex flex-wrap bg-dark align-items-center" v-for="(city, index1) in shownForecast" v-bind:key="index1" style="minHeight: 350px; maxHeight: 400px; minWidth: 500px; margin-bottom: 5%; border-radius: 15px">
+      <div class="col-6 d-flex flex-column bg-secondary p-4" :style="{ backgroundImage: 'url(' + require('@/images/morning.jpg') + ')', backgroundRepeat: 'no-repeat', backgroundSize: 'cover', width: '220px', minHeight: '360px', borderRadius: '15px'}">
+        <p class="h2 text-light">{{ dayOfTheWeek }}</p>
+        <p class="h5">{{ date }}</p>
+        <p><i class="fas fa-map-marker-alt"></i> {{ names[shownForecast.indexOf(city)] }}</p>
+        <p class="text-light" style="font-size: 46px"> {{ Math.floor(city[shownForecast.indexOf(city) - shownForecast.indexOf(city)].temperature - 273.15) }} &deg;C</p>
+        <p class="text-light" style="font-size: 26px"> {{ city[shownForecast.indexOf(city) - shownForecast.indexOf(city)].mainDescription }}</p>
+      </div>
+      <div class="col-6 d-flex flex-column justify-content-start p-4" style="minHeight: 300px">
+        <p class="text-info">Description: {{ city[shownForecast.indexOf(city) - shownForecast.indexOf(city)].sideDescription }}</p>
+        <p class="text-info">Humidity: {{ city[shownForecast.indexOf(city) - shownForecast.indexOf(city)].humidity }}</p>
+        <p class="text-info">Wind: {{ city[shownForecast.indexOf(city) - shownForecast.indexOf(city)].wind }} km/h</p>
+        <span v-for="(temperature, tempIndex2) in shownTemperatures[shownForecast.indexOf(city)]" v-bind:key="tempIndex2">
+          <div class="d-flex align-items-center">
+            <i class="fas fa-cloud mr-2 text-light"></i><font class="text-light">{{ Math.floor(temperature - 273.15) }} &deg;C</font>
+          </div>
+        </span>
+      </div>
     </div>
   </div>
   </div>
@@ -21,7 +37,10 @@ export default {
   data() {
     return {
       forecast: [],
+      temperatures: [],
+      shownTemperatures: [],
       shownForecast: [],
+      names: [],
       day: {
         temperature: "",
         humidity: "",
@@ -81,8 +100,8 @@ export default {
       fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + city.name + '&appid=5874315343445491bc07e2559123999e')                                                                       
       .then(response => response.json())
       .then(data => {
+        this.names.push(city.name)
         let i = 0;
-        console.log(data.list)
         while(i <= data.list.length) {
           this.day = {
             temperature: data.list[i].main.temp,
@@ -91,6 +110,7 @@ export default {
             mainDescription: data.list[i].weather[0].main,
             sideDescription: data.list[i].weather[0].description
           }
+          this.temperatures.push(this.day.temperature)
           this.forecast.push(this.day)
           this.resetDayData();
           if(i == 0) {
@@ -100,12 +120,19 @@ export default {
           }
         }
         this.shownForecast.push(this.forecast)
+        this.shownTemperatures.push(this.temperatures)
         this.forecast = []
+        this.temperatures = []
       })
     })
     this.getDateInfo();
     console.log(this.shownForecast)
+    console.log(this.shownTemperatures)
     console.log(this.date)
   }
 }
 </script>
+
+<style scoped>
+
+</style>
